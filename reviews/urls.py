219224acb813950cf4b39eaa444eeae1acc827fa
Vasily Kuznetsov <kvas.it@gmail.com>
2015-08-06 23:07:53 +0200

    Convert answer sheet to a form and enable saving.

--- reviews/urls.py
+++ reviews/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # ex: /reviews/answer-sheet/5/
     url(r'^answer-sheet/(?P<answer_sheet_id>[0-9]+)/$',
         views.answer_sheet, name='answer'),
-    # ex: /reviews/answer-sheet/5/4/save/
-    url(r'^(?P<answer_sheet_id>[0-9]+)/(?P<question_id>[0-9]+)/save/$',
-        views.save_answer, name='save-answer'),
+    # ex: /reviews/answer-sheet/5/save/
+    url(r'^answer-sheet/(?P<answer_sheet_id>[0-9]+)/save/$',
+        views.save_answers, name='save-answers'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 
@@ -60,7 +61,36 @@ def answer_sheet(request, answer_sheet_id):
     })
 
 
+def store_answer_data(answer_sheet, answer_values):
+    """Store answer values for the answer sheet."""
+    old_answers = answer_sheet.get_answers()
+    review_template = answer_sheet.review.review_template
+    for group in review_template.get_question_groups():
+        for question in group.get_questions():
+            answer_value = answer_values.get(question.id, '')
+            if question.id in old_answers:
+                answer = old_answers[question.id]
+                answer.value = answer_value
+            else:
+                answer = models.Answer(
+                        answer_sheet=answer_sheet,
+                        question=question,
+                        value=answer_value)
+            answer.save()
+
+
 @login_required
-def save_answer(request, answer_sheet_id, question_id):
-    return HttpResponse('Saving answer %s on answer sheet %s' %
-            (question_id, answer_sheet_id))
+def save_answers(request, answer_sheet_id):
+    answer_sheet = get_object_or_404(models.AnswerSheet, pk=answer_sheet_id)
+    if answer_sheet.owner != request.user:
+        raise PermissionDenied('Answers belong to another user')
+    answer_values = {}
+    for key, value in request.POST.items():
+        if not key.startswith('answer_'):
+            continue
+        t = key.split('_')
+        question_id = int(t[1])  # XXX: add error handling.
+        answer_values[question_id] = value
+    store_answer_data(answer_sheet, answer_values)
+    redirect_url = reverse('reviews:answer', args=(answer_sheet.id,))
+    return HttpResponseRedirect(redirect_url)
