3cf7730471ebc717d6f8490059f2b6c0b2bc70cf
Vasily Kuznetsov <kvas.it@gmail.com>
2015-08-06 21:12:37 +0200

    Add answer sheet display.

--- reviews/views.py
+++ reviews/views.py
@@ -21,16 +21,48 @@ def review(request, review_id):
     if review.owner != request.user:
         raise PermissionDenied('Review belongs to another user')
     answer_sheets = (models.AnswerSheet.objects
-            .filter(review=review)
-            .exclude(owner=request.user))
+        .filter(review=review)
+        .exclude(owner=request.user))
     return render(request, 'reviews/review.html', {
         'review': review, 'answer_sheets': answer_sheets
     })
 
 
+def prepare_answer_data(answer_sheet):
+    answers = {}
+    for answer in models.Answer.objects.filter(answer_sheet=answer_sheet):
+        answers[answer.question.id] = answer
+    review_template = answer_sheet.review.review_template
+    answer_data = []
+    for group in (models.QuestionGroup.objects
+                    .filter(review_template=review_template)
+                    .order_by('order')):
+        group_rec = {
+            'question_group': group,
+            'questions': []
+        }
+        answer_data.append(group_rec)
+        for question in (models.Question.objects
+                            .filter(question_group=group)
+                            .order_by('order')):
+            question_rec = {
+                'question': question,
+                'answer': answers.get(question.id, None)
+            }
+            group_rec['questions'].append(question_rec)
+    return answer_data
+
+
 @login_required
 def answer_sheet(request, answer_sheet_id):
-    return HttpResponse('Answer sheet %s' % answer_sheet_id)
+    answer_sheet = get_object_or_404(models.AnswerSheet, pk=answer_sheet_id)
+    if answer_sheet.owner != request.user:
+        raise PermissionDenied('Answers belong to another user')
+    return render(request, 'reviews/answer_sheet.html', {
+        'review': answer_sheet.review,
+        'answer_sheet': answer_sheet, 
+        'answer_data': prepare_answer_data(answer_sheet)
+    })
 
 
 @login_required
