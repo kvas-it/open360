8058f06ae0c1e9e15c10cb14f85bd8289fe8c54a
Vasily Kuznetsov <kvas.it@gmail.com>
2015-08-06 17:57:10 +0200

    Add models to the admin interface.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+        ReviewTemplate,
+        ReviewerClass,
+        QuestionGroup,
+        Question,
+        Review,
+        AnswerSheet,
+        Answer)
+
+
+admin.site.register(ReviewTemplate)
+admin.site.register(ReviewerClass)
+admin.site.register(QuestionGroup)
+admin.site.register(Question)
+admin.site.register(Review)
+admin.site.register(AnswerSheet)
+admin.site.register(Answer)
